81d80b65ba48b2291eedc16bf2cb387208b96d7a
MISTUser <MISTUser@lab8L4JUY>
2024-01-28 18:18:40 -0500

    Add project files.

---
+++ Homework1Southwell/Pages/Searchresults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Homework1Southwell.Pages
+{
+    public class SearchresultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
